e3cb6cd346ab119702a6eba582f47034c0e1eb1d
Keir Nellyer <keir@nellyer.co.uk>
2016-04-15 19:12:49 +0100

    Footer improvements

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
 <h1>BrassSnakeShop</h1>
 <p>Welcome to the BrassSnakeShop, this will soon be a fully fledged store.</p>
 <br />
-<hr>
-Git Revision - <pre><?= shell_exec('git rev-parse HEAD') ?></pre><br \>
-Last Commit by - <?= shell_exec('git log -1 --pretty=%an') ?>
+<div id="footer" align="center">
+    <hr>
+    Git Revision <code><?= shell_exec('git rev-parse HEAD') ?></code><br />
+    Last Commit by <?= shell_exec('git log -1 --pretty=%an') ?>
+</div>
 </body>
 </html>
