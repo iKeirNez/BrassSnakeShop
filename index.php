29fe8cb3403552abfda2cd722d287c351ca0116f
Keir Nellyer <keir@nellyer.co.uk>
2016-04-15 15:46:18 +0100

    Wrote placeholder index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>BrassSnakeShop</title>
+</head>
+<body>
+<h1>BrassSnakeShop</h1>
+<p>Welcome to the BrassSnakeShop, this will soon be a fully fledged store.</p>
+</body>
+</html>
