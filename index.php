ee574e39cb0c390bb15b776b313471e2cdd2d208
Keir Nellyer <keir@nellyer.co.uk>
2016-04-15 19:04:45 +0100

    Get git information and display in footer

--- index.php
+++ index.php
@@ -5,5 +5,9 @@
 <body>
 <h1>BrassSnakeShop</h1>
 <p>Welcome to the BrassSnakeShop, this will soon be a fully fledged store.</p>
+<br />
+<hr>
+Git Revision - <pre><?= shell_exec('git rev-parse HEAD') ?></pre><br \>
+Last Commit by - <?= shell_exec('git log -1 --pretty=%an') ?>
 </body>
 </html>
