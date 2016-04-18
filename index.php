00e395e54e2129819a91807165ed81d7a2c883e3
Keir Nellyer <keir@nellyer.co.uk>
2016-04-18 19:35:41 +0100

    Allow tunneled connections to the database

--- index.php
+++ index.php
@@ -2,16 +2,17 @@
 
 /**
  * @param SplFileObject $configFile
+ * @param resource $tunnel
  * @return mysqli
  */
-function getConnection($configFile) {
+function getDbConnection($configFile, $tunnel) {
     $host = $configFile->fgets();
     $port = $configFile->fgets();
     $database = $configFile->fgets();
     $username = $configFile->fgets();
     $password = null; // TODO handle pass
 
-    $connection = new mysqli($host, $username, $password, $database, $port);
+    $connection = new mysqli($host, $username, $password, $database, $port, $tunnel);
     
     if ($connection->connect_error) {
         die('<h1>MYSQL CONNECT FAILED.</h1><p>' . $connection->connect_error . '</p>');
@@ -20,7 +21,41 @@ function getConnection($configFile) {
     return $connection;
 }
 
-$connection = getConnection(new SplFileObject('config/mysql.dat'));
+/**
+ * @param SplFileObject $configFile
+ * @return resource ssh session
+ */
+function getSshConnection($configFile) {
+    $host = $configFile->fgets();
+    $port = $configFile->fgets();
+
+    $ssh = ssh2_connect($host, $port);
+
+    $username = $configFile->fgets();
+    $password = $configFile->fgets();
+
+    if (!ssh2_auth_password($ssh, $username, $password)) {
+        die('<h1>SSH authentication failed</h1>');
+    }
+
+    return $ssh;
+}
+
+/**
+ * @param resource $ssh
+ * @param SplFileObject $configFile
+ * @return resource tunnel
+ */
+function getTunnelConnection($ssh, $configFile) {
+    $destination = $configFile->fgets();
+    $port = $configFile->fgets();
+    
+    return ssh2_tunnel($ssh, $destination, $port);
+}
+
+$ssh = getSshConnection(new SplFileObject('config/ssh.dat'));
+$tunnel = getTunnelConnection($ssh, new SplFileObject('config/tunnel.dat'));
+$connection = getDbConnection(new SplFileObject('config/mysql.dat'), $tunnel);
 
 ?>
 
