a930ec00b513f5a42cccf74ac39aeb2953ad0c4d
Keir Nellyer <keir@nellyer.co.uk>
2016-04-25 14:48:56 +0100

    Always show PHP errors

--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,8 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 define('GITHUB_USER_PREFIX', 'https://github.com/');
 define('GITHUB_COMMIT_PREFIX', 'https://github.com/iKeirNez/BrassSnakeShop/commit/');
