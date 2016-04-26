21e9291ed1a461918d8c3e14e3ea94f4af834630
Keir Nellyer <keir@nellyer.co.uk>
2016-04-26 21:46:52 +0100

    Could probably do with an extra break here

--- includes/footer.php
+++ includes/footer.php
@@ -12,6 +12,6 @@ $gitUser = Git::getLastCommitter();
 <div id="footer" align="center">
     <hr>
     Last Commit by <?= Git::getAuthorDisplayCode($gitUser) ?><br />
-    <code><a href="<?= GITHUB_COMMIT_PREFIX . $commitHash ?>" target="_blank"><?= $commitHash ?></a></code><br />
+    <code><a href="<?= GITHUB_COMMIT_PREFIX . $commitHash ?>" target="_blank"><?= $commitHash ?></a></code><br /><br />
     &copy; 2016 <?= WEBSITE_NAME ?>
 </div>
