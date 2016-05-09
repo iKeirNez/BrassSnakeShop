b542907cd4fe2140ec056fb5a2842c6d30e640cf
Keir Nellyer <keir@nellyer.co.uk>
2016-05-09 16:15:01 +0100

    Implement register page

--- includes/functions.php
+++ includes/functions.php
@@ -22,3 +22,47 @@ function parseValue($strValue) {
 
     return $value;
 }
+
+/**
+ * Checks if a variable is empty and if so, displays an error message.
+ *
+ * @param $var
+ * @param string $message
+ */
+function checkInputVar($var, $message) {
+    if (empty($var)) {
+        die($message);
+    }
+}
+
+/**
+ * Checks if a row exists with a column matching the search criteria.
+ *
+ * @param mysqli $mysqli
+ * @param string $column
+ * @param string $type mysql value type, for example 'i' for integer
+ * @param object $value
+ * @return bool
+ */
+function checkRowExists($mysqli, $column, $type, $value) {
+    $statement = $mysqli->prepare('SELECT COUNT(*) AS total FROM users WHERE ' . $column . ' = ?;');
+    $statement->bind_param($type, $value);
+    $statement->execute();
+    $statement->bind_result($total);
+    return $total > 0;
+}
+
+/**
+ * @param string $password
+ * @param string $salt
+ * @return string
+ */
+function hashPassword($password, $salt) {
+    $hash = $password;
+
+    for ($i = 0; $i < 65536; $i++) {
+        $hash = hash('sha512', $hash . $salt);
+    }
+
+    return $hash;
+}
---
+++ register.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once ('includes/config.php');
+require_once ('includes/db.php');
+require_once ('includes/functions.php');
+
+if (!empty($_POST)) {
+    checkInputVar($_POST['username'], 'Please enter a username.');
+    checkInputVar($_POST['email'], 'Please enter an email address.');
+    checkInputVar($_POST['password'], 'Please enter a password.');
+    checkInputVar($_POST['first_name'], 'Please enter your first name.');
+    checkInputVar($_POST['last_name'], 'Please enter your last name.');
+    
+    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        die('Please enter a valid email address.');
+    }
+
+    $mysqli = getMainDbConnection();
+
+    if (checkRowExists($mysqli, 'username', 's', $_POST['username'])) {
+        die('That username is already in use.');
+    }
+
+    if (checkRowExists($mysqli, 'email', 's', $_POST['email'])) {
+        die('That email address is already in use.');
+    }
+
+    $salt = generateRandomSalt();
+    $password = hashPassword($_POST['password'], $salt);
+
+    $statement = $mysqli->prepare('INSERT INTO users (username, first_name, last_name, email, salt, password_hash) VALUES (?, ?, ?, ?, ?, ?);');
+    $statement->bind_param('s', $_POST['username']);
+    $statement->bind_param('s', $_POST['first_name']);
+    $statement->bind_param('s', $_POST['last_name']);
+    $statement->bind_param('s', $_POST['email']);
+    $statement->bind_param('s', $salt);
+    $statement->bind_param('s', $hash);
+
+    header('Location: login.php');
+    die('Redirecting...');
+}
+
+?>
+
+<h2>Register</h2>
+
+<form action="register.php" method="post">
+    First Name:<br />
+    <input type="text" name="first_name" value="" />
+    <br /><br />
+    Last Name:<br />
+    <input type="text" name="last_name" value="" />
+    <br /><br />
+    Username:<br />
+    <input type="text" name="username" value="" />
+    <br /><br />
+    E-Mail:<br />
+    <input type="text" name="email" value="" />
+    <br /><br />
+    Password:<br />
+    <input type="password" name="password" value="" />
+    <br /><br />
+    <input type="submit" value="Register" />
+</form>
