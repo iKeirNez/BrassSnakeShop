f27de9327aa511f7ee483c8d6c1fd36da2cef4cd
Keir Nellyer <keir@nellyer.co.uk>
2016-05-16 15:39:06 +0100

    Redirect to login page after registering

--- register.php
+++ register.php
@@ -32,7 +32,7 @@ if (!empty($_POST)) {
     $statement->bind_param('ssssss', $_POST['username'], $_POST['first_name'], $_POST['last_name'], $_POST['email'], $salt, $password);
     $statement->execute();
 
-    //header('Location: login.php');
+    header('Location: login.php');
     die('Redirecting...');
 }
 
