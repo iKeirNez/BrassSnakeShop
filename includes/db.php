bb60567c3f2699d3fb01722dd4cf2a221966bca4
Keir Nellyer <keir@nellyer.co.uk>
2016-05-16 15:58:29 +0100

    Re-use same mysql connection

--- includes/db.php
+++ includes/db.php
@@ -2,15 +2,21 @@
 
 require_once('config.php');
 
+$connection = null;
+
 /**
  * Connects to a database with settings defined in the configuration.
  * @return mysqli
  */
 function getMainDbConnection() {
-    $connection = new mysqli(MYSQL_HOST, MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DATABASE, MYSQL_PORT);
+    global $connection;
 
-    if (mysqli_connect_errno()) {
-        die('<h1>MYSQL CONNECT FAILED.</h1><p>' . mysqli_connect_error() . '</p>');
+    if ($connection == null) {
+        $connection = new mysqli(MYSQL_HOST, MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DATABASE, MYSQL_PORT);
+
+        if (mysqli_connect_errno()) {
+            die('<h1>MYSQL CONNECT FAILED.</h1><p>' . mysqli_connect_error() . '</p>');
+        }
     }
 
     return $connection;
