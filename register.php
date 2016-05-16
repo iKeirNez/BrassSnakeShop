41f1fffff500bc096c665e3d49b90cc43e5f5073
Keir Nellyer <keir@nellyer.co.uk>
2016-05-16 15:40:45 +0100

    Only redirect after registering if it was successful

--- register.php
+++ register.php
@@ -30,10 +30,11 @@ if (!empty($_POST)) {
 
     $statement = $mysqli->prepare('INSERT INTO users (username, first_name, last_name, email, salt, password_hash) VALUES (?, ?, ?, ?, ?, ?);');
     $statement->bind_param('ssssss', $_POST['username'], $_POST['first_name'], $_POST['last_name'], $_POST['email'], $salt, $password);
-    $statement->execute();
 
-    header('Location: login.php');
-    die('Redirecting...');
+    if ($statement->execute()) {
+        header('Location: login.php');
+        die('Redirecting...');
+    }
 }
 
 include('includes/header.php');
