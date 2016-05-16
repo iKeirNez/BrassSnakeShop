bd9b0e4f154392a439c167ddcbc957e4b9458df0
Brian Cleland <AllBranFlakes@users.noreply.github.com>
2016-05-16 15:11:55 +0100

    Added sample form
    Testing new form code on website - will be removed

--- index.php
+++ index.php
@@ -17,6 +17,31 @@ $connection = getMainDbConnection();
 
 <p>Welcome to the <?= WEBSITE_NAME ?>, this will soon be a fully fledged store.</p>
 
+<!-- Following code is a test - BC 16/05/16 15:09-->
+<form action="#">
+  
+  <fieldset>
+    
+    <legend>Sign In</legend>
+    
+    <label for="username" class="vhide">Username</label>
+    <input id="username" name="username" type="text" placeholder="Username" required minlength="2">
+    
+    <label for="password" class="vhide">Password</label>
+    <input id="password" name="password" type="text" placeholder="Password" required minlength="6">
+    
+    <input type="checkbox" name="remember" id="remember" class="vhide">
+    <label for="remember">
+      <i class="octicon octicon-check"></i> Remember all the things
+    </label>
+        
+    <button class="signin">Sign in</button>
+    
+  </fieldset>
+  
+</form>
+<!-- END OF TEST - BC 16/05/16 15:09-->
+
 <?php include('includes/footer.php') ?>
 </body>
 </html>
