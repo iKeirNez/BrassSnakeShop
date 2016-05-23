c5a4d24e343b2846e9d0f0850c0db26f81283fc9
Brian Cleland <AllBranFlakes@users.noreply.github.com>
2016-05-23 15:40:52 +0100

    Addition of div
    Addition of div sections for fixed footer operation
    Also tidied code

--- index.php
+++ index.php
@@ -11,47 +11,56 @@ session_start();
 ?>
 
 <html>
-<head>
+    <head>
     <title><?= WEBSITE_NAME ?></title>
     <!-- ADDITION OF EXTERNAL ASSETS - BC 16/05/16 15:33 -->
     <link rel="stylesheet" type="text/css" href="includes/css/main.css">
     <script src="includes/js/main.js"></script>
     <!-- END OF ADDITION - BC 16/05/16 15:33 -->
-</head>
-<body>
-<?php include('includes/header.php') ?>
+    </head>
+    
+    <body>
+        <div id="wrapper">
+        	<div id="header">
+            <?php include('includes/header.php') ?>
+	        </div>
+	    <div id="content">
+            <?php if (isset($_SESSION['user_id'])): ?>
+            <p>Welcome back, <?= $_SESSION['username'] ?></p>
+            <?php else: ?>
+            <!--    Addition of forms for login 
+                    CSS location        -  "//includes/css/main.css"
+                    JavaScript location -  "//includes/js/main.js" - BC 16/05/16 15:09 -->
+            <form action="login.php" method="post">
 
-<?php if (isset($_SESSION['user_id'])): ?>
-    <p>Welcome back, <?= $_SESSION['username'] ?></p>
-<?php else: ?>
-    <!--    Addition of forms for login 
-            CSS location        -  "//includes/css/main.css"
-            JavaScript location -  "//includes/js/main.js" - BC 16/05/16 15:09 -->
-    <form action="login.php" method="post">
+                <fieldset>
 
-        <fieldset>
+                    <legend>Sign In</legend>
 
-            <legend>Sign In</legend>
+                    <label for="username" class="vhide">Username</label>
+                    <input id="username" name="username" type="text" placeholder="Username" required minlength="2">
 
-            <label for="username" class="vhide">Username</label>
-            <input id="username" name="username" type="text" placeholder="Username" required minlength="2">
+                    <label for="password" class="vhide">Password</label>
+                    <input id="password" name="password" type="password" placeholder="Password" required minlength="6">
 
-            <label for="password" class="vhide">Password</label>
-            <input id="password" name="password" type="password" placeholder="Password" required minlength="6">
+                    <input type="checkbox" name="remember" id="remember" class="vhide">
+                    <label for="remember">
+                    
+                    <i class="octicon octicon-check"></i> Remember all the things
+                
+                    </label>
 
-            <input type="checkbox" name="remember" id="remember" class="vhide">
-            <label for="remember">
-                <i class="octicon octicon-check"></i> Remember all the things
-            </label>
+                    <button class="signin">Sign in</button>
 
-            <button class="signin">Sign in</button>
+                </fieldset>
 
-        </fieldset>
-
-    </form>
-    <!-- END OF ADDITION - BC 16/05/16 15:09 -->
-<?php endif; ?>
-
-<?php include('includes/footer.php') ?>
-</body>
+            </form>
+                <!-- END OF ADDITION - BC 16/05/16 15:09 -->
+                <?php endif; ?>
+	        </div>
+		    <div id="footer">
+                <?php include('includes/footer.php') ?>
+            </div>
+        </div>
+    </body>
 </html>
