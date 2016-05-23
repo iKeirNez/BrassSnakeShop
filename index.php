e5f3c92d8d5d0f29ef548a8ee408c912c9e07f47
Brian Cleland <AllBranFlakes@users.noreply.github.com>
2016-05-23 16:46:11 +0100

    Update to password min length
    reduced required password min-length to remove log in problems

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ session_start();
                     <input id="username" name="username" type="text" placeholder="Username" required minlength="2">
 
                     <label for="password" class="vhide">Password</label>
-                    <input id="password" name="password" type="password" placeholder="Password" required minlength="6">
+                    <input id="password" name="password" type="password" placeholder="Password" required minlength="1">
 
                     <input type="checkbox" name="remember" id="remember" class="vhide">
                     <label for="remember">
