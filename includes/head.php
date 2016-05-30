fef7fb5b76b6a59eaea5df276b850b225f85bc00
Brian Cleland <AllBranFlakes@users.noreply.github.com>
2016-05-30 16:36:12 +0100

    updated assets in head

--- includes/head.php
+++ includes/head.php
@@ -4,4 +4,10 @@
 <link rel="stylesheet" type="text/css" href="/includes/css/sfnavbar.css">
 <link rel="stylesheet" type="text/css" href="/includes/css/main.css">
 <script src="/includes/js/main.js"></script>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="/js/bootstrap.js"></script>
 <!-- END OF ADDITION - BC 16/05/16 15:33 -->
