c6e79d2de5ee00413449a8ca7891950e601415d1
Brian Cleland <AllBranFlakes@users.noreply.github.com>
2016-05-30 16:37:08 +0100

    minor update

--- includes/head.php
+++ includes/head.php
@@ -9,5 +9,5 @@
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-<script src="/js/bootstrap.js"></script>
+<script src="/includes/js/bootstrap.js"></script>
 <!-- END OF ADDITION - BC 16/05/16 15:33 -->
