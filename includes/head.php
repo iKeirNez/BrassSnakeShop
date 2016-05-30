7a469609b06f5f733b75c3aae82827615cc76180
Keir Nellyer <keir@nellyer.co.uk>
2016-05-30 16:12:37 +0100

    Move common head stuff over to head.php and include.

---
+++ includes/head.php
@@ -0,0 +1,7 @@
+<!-- ADDITION OF EXTERNAL ASSETS - BC 16/05/16 15:33 -->
+<link rel="stylesheet" type="text/css" href="/includes/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="/includes/css/ftnavbar.css">
+<link rel="stylesheet" type="text/css" href="/includes/css/sfnavbar.css">
+<link rel="stylesheet" type="text/css" href="/includes/css/main.css">
+<script src="/includes/js/main.js"></script>
+<!-- END OF ADDITION - BC 16/05/16 15:33 -->
--- index.php
+++ index.php
@@ -14,14 +14,7 @@ session_start();
 <html>
     <head>
     <title><?= WEBSITE_NAME ?></title>
-    <!-- ADDITION OF EXTERNAL ASSETS - BC 16/05/16 15:33 -->
-    <link rel="stylesheet" type="text/css" href="includes/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="includes/css/ftnavbar.css">
-    <link rel="stylesheet" type="text/css" href="includes/css/sfnavbar.css">
-    <link rel="stylesheet" type="text/css" href="includes/css/main.css">
-    <script src="includes/js/main.js"></script>
-    <!-- END OF ADDITION - BC 16/05/16 15:33 -->
-
+    <?php include('includes/head.php') ?>
     </head>
     
     <body>
