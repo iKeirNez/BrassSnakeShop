8181ccfd33b0af70cffc4d3c9286b17cf70f333c
Brian Cleland <AllBranFlakes@users.noreply.github.com>
2016-05-30 14:38:17 +0100

    change to local links
    Local links used for CSS and JS bootstrap

--- index.php
+++ index.php
@@ -16,15 +16,7 @@ session_start();
     <title><?= WEBSITE_NAME ?></title>
     <!-- ADDITION OF EXTERNAL ASSETS - BC 16/05/16 15:33 -->
     <link rel="stylesheet" type="text/css" href="includes/css/main.css">
- <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="navbar-fixed-top.css" rel="stylesheet">
-
+    <link rel="stylesheet" type="text/css" href="includes/css/ftnavbar.css">
     <link rel="stylesheet" type="text/css" href="includes/css/sfnavbar.css">
     <script src="includes/js/main.js"></script>
     <!-- END OF ADDITION - BC 16/05/16 15:33 -->
