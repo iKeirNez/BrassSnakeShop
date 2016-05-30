7ce37e23dcce515ea49000a6d4ef8da8a64ae7e6
Brian Cleland <AllBranFlakes@users.noreply.github.com>
2016-05-30 16:32:39 +0100

    bootstrap java for dropdowns

--- index.php
+++ index.php
@@ -60,5 +60,12 @@ session_start();
                 	<?php include('includes/footer.php') ?>
             	</footer>
         </div>
+        
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="/includes/js/bootstrap.js"></script>
     </body>
 </html>
