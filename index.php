5f6151cd66a97d3171b8fb92cd5050823784abfa
Brian Cleland <AllBranFlakes@users.noreply.github.com>
2016-05-30 14:49:37 +0100

    Changed CSS load order
    placed main.css at end of load order to ensure it loads correctly

--- index.php
+++ index.php
@@ -15,16 +15,13 @@ session_start();
     <head>
     <title><?= WEBSITE_NAME ?></title>
     <!-- ADDITION OF EXTERNAL ASSETS - BC 16/05/16 15:33 -->
-    <link rel="stylesheet" type="text/css" href="includes/css/main.css">
-    <link rel="stylesheet" type="text/css" href="includes/css/boostrap.css">
+    <link rel="stylesheet" type="text/css" href="includes/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="includes/css/ftnavbar.css">
     <link rel="stylesheet" type="text/css" href="includes/css/sfnavbar.css">
+    <link rel="stylesheet" type="text/css" href="includes/css/main.css">
     <script src="includes/js/main.js"></script>
     <!-- END OF ADDITION - BC 16/05/16 15:33 -->
-    
-    <!-- stuff after this point is a test -->
 
-    <!-- end of test -->
     </head>
     
     <body>
