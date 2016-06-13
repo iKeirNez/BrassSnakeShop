bd099538dcb29a26d5ac7425fdec12e7d2c0d10b
Keir Nellyer <keir@nellyer.co.uk>
2016-06-13 14:08:53 +0100

    Add sample table

--- index.php
+++ index.php
@@ -12,20 +12,32 @@ session_start();
 ?>
 
 <html>
-    <head>
+<head>
     <title><?= WEBSITE_NAME ?></title>
     <?php include('includes/head.php') ?>
-    </head>
-    
-    <body>
-        <div id="wrapper">
-        	<div id="header">
-            <?php include('includes/header.php') ?>
-	        </div>
-	    <div id="content">
-            <?php if (isset($_SESSION['user'])): ?>
+</head>
+
+<body>
+<div id="wrapper">
+    <div id="header">
+        <?php include('includes/header.php') ?>
+    </div>
+    <div id="content">
+        <?php if (isset($_SESSION['user'])): ?>
             <p>Welcome back, <?= $_SESSION['user']->getFirstName(); ?></p>
-            <?php else: ?>
+
+            <table>
+                <tr>
+                    <td><b>Header 1</b></td>
+                    <td><b>Header 2</b></td>
+                </tr>
+
+                <tr>
+                    <td>Content 1</td>
+                    <td>Content 2</td>
+                </tr>
+            </table>
+        <?php else: ?>
             <!--    Addition of forms for login 
                     CSS location        -  "//includes/css/main.css"
                     JavaScript location -  "//includes/js/main.js" - BC 16/05/16 15:09 -->
@@ -43,9 +55,9 @@ session_start();
 
                     <input type="checkbox" name="remember" id="remember" class="vhide">
                     <label for="remember">
-                    
-                    <i class="octicon octicon-check"></i> Remember all the things
-                
+
+                        <i class="octicon octicon-check"></i> Remember all the things
+
                     </label>
 
                     <button class="signin">Sign in</button>
@@ -53,13 +65,13 @@ session_start();
                 </fieldset>
 
             </form>
-                <!-- END OF ADDITION - BC 16/05/16 15:09 -->
-                <?php endif; ?>
-	        </div>
-	        <footer class="footer">
-                	<?php include('includes/footer.php') ?>
-            	</footer>
-        </div>
-       
-    </body>
+            <!-- END OF ADDITION - BC 16/05/16 15:09 -->
+        <?php endif; ?>
+    </div>
+    <footer class="footer">
+        <?php include('includes/footer.php') ?>
+    </footer>
+</div>
+
+</body>
 </html>
